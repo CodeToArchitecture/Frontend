ae540180b7559942af1b62ee65dbdf6e11540ded
Rishabh Malviya <rishabh.malviya@gmail.com>
2024-03-16 19:22:20 -0700

    more updates to skeleton

--- app.py
+++ app.py
@@ -1,40 +1,45 @@
+import requests
 import streamlit as st
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
 
-def get_api_response(repo_link):
-    # Your actual API call to get the iframe or relevant information
-    path = './rick.png'
-    return path
-
-def chat_with_bot(user_message):
-    # Your actual chatbot backend call
-    return "Echo: " + user_message
-
-if 'chat_history' not in st.session_state:
-    st.session_state.chat_history = []
-
-def send_message():
-    user_message = st.session_state.user_message
-    if user_message:  # Check if the message is not empty
-        st.session_state.chat_history.append(f"You: {user_message}")
-        bot_response = chat_with_bot(user_message)
-        st.session_state.chat_history.append(f"Bot: {bot_response}")
-        # Clear the input
-        st.session_state.user_message = ""
-
-st.title("Code To Architecture")
+####################
+# DOM
+####################
+st.title("Codebase Explainer")
 
 repo_link = st.text_input("Enter GitHub repository link:", key="repo_link")
 submit_repo = st.button("Submit GitHub Link")
 
-if submit_repo and repo_link:
-    st.session_state.iframe_html = get_api_response(repo_link)
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-if 'iframe_html' in st.session_state and st.session_state.iframe_html:
-    st.image('./rick.png')
+if 'chat_history' not in st.session_state:
+    st.session_state.chat_history = []
+
+
+####################
+# Behaviour
+####################
+if submit_repo and repo_link:
+    populate_files()
+    st.image(get_image())
+
+def populate_file():
+    # Call Node populate_files
+    raise NotImplementedError
+
+def get_architecture_diagram():
+    # Generate prompt from files
+    # Send context and prompt to get_image 
+    raise NotImplementedError
+
+
+
+
+
+
+
+
 
 
 # Display chat messages from history on app rerun
