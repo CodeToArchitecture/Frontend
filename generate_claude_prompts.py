a2b7067c5d3c0f34aa29af179aa8f6affe6d6a27
paritoshborkar <paritoshpborkar@gmail.com>
2024-03-16 20:26:45 -0700

    Create system prompt for chat

--- generate_claude_prompts.py
+++ generate_claude_prompts.py
@@ -18,36 +18,48 @@ def create_user_prompt_for_image() -> None:
 
     Only output the XML content, do no output any English."
     """
-    user_prompt_file = open(user_prompt_filename, "a")
-    tf_files = [tf_filename for tf_filename in os.listdir(path_to_code_files) \
-                if (isfile(join(path_to_code_files, tf_filename)) and \
-                (tf_filename.endswith(".tf") or tf_filename.endswith("tfvars")))]
+    tf_files = _get_code_filenames()
 
     with open(user_prompt_filename, "a") as user_prompt_file:
         user_prompt_file.write("Now generate XML output for this terraform code. Make sure to close all the XML tags you open:\n")
         user_prompt_file.write(f"{divider_string}\n")
 
-        for tf_filename in tf_files:
-            # Open each tf file
-            with open(join(path_to_code_files, tf_filename), "r") as tf_file:
-            # Write title of tf file in prompt file
-            # Write tf file contents in prompt file
-            # Close the tf file
-                user_prompt_file.write(f"Title: {tf_filename}\n")
-                user_prompt_file.writelines(tf_file.readlines())
-                
+        _write_code_files_to_prompt_file(prompt_file=user_prompt_file, code_filenames=tf_files)
 
         user_prompt_file.write(f"{divider_string}\n")
         user_prompt_file.write("Only output the XML content, do no output any English.")
-    
-    
 
 
-def create_system_prompt():
+def create_system_prompt_for_chat():
     """
     Creates prompt with Terraform files to XML examples
+
+This is the code base:
+
+{code base files}
     """
-    pass
+    tf_files = _get_code_filenames()
+
+    with open(system_prompt_filename, "a") as prompt_file:
+        prompt_file.write("You are an assistant. Help the user understand with below code context.\n")
+        prompt_file.write("These are the code files:\n")
+        prompt_file.write(f"{divider_string}\n")
+
+        _write_code_files_to_prompt_file(prompt_file=prompt_file, code_filenames=tf_files)
+
+        prompt_file.write(f"{divider_string}\n")
 
 
-create_user_prompt_for_image()
+def _get_code_filenames():
+    return [tf_filename for tf_filename in os.listdir(path_to_code_files) \
+                if (isfile(join(path_to_code_files, tf_filename)) and \
+                (tf_filename.endswith(".tf") or tf_filename.endswith("tfvars")))]
+
+def _write_code_files_to_prompt_file(prompt_file, code_filenames: str):
+    for tf_filename in code_filenames:
+        # Open each tf file
+        with open(join(path_to_code_files, tf_filename), "r") as tf_file:
+        # Write title of tf file in prompt file
+            prompt_file.write(f"Title: {tf_filename}\n")
+        # Write tf file contents in prompt file
+            prompt_file.writelines(tf_file.readlines())
